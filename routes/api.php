6f1561f74dfc8a2a734725dd398627f819892c55
Kevin Quinn <kevin.quinn@totalserversolutions.com>
2021-11-19 13:51:11 -0500

    Remove commented out code

--- routes/api.php
+++ routes/api.php
@@ -21,19 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::middleware('auth:sanctum')
-//    ->get('/notes', function (Request $request) {
-//        $notes = Note::all()
-//            ->where(['user_id', $request->user()->id]);
-//
-//        return new NoteCollection($notes);
-//    })
-//    ->get('notes/{id}', function (Request $request) {
-//        $note = Note::where(['user_id', $request->user()->id]);
-//
-//        return NoteResource($note);
-//    });
-
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/notes', function (Request $request) {
         $notes = Note::all()
